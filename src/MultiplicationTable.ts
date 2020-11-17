d4b19516bf4dbca43fab1da99f8254eadf341248
Lin LIU <lliu41@slb.com>
2020-11-17 19:06:37 +0800

    Feat: Add validation for rule - start <= end

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,6 @@
 export class MultiplicationTable {
   public render(start: number, end: number): string{
+    if(start > end) return ''
     return '1*1=1'
   }
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -13,4 +13,17 @@ describe('Multiplication Table', () => {
     //then
     expect(rendered).toBe('1*1=1')
   })
+
+  it('should return empty string if start is larger than end', ()=> {
+    //given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 1
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
+  })
 })
