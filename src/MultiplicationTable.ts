6a2a09bedff31e09d6344ce7c844f3e62777b24b
Lin LIU <lliu41@slb.com>
2020-11-17 20:50:23 +0800

    Feat: Complete implementation for rendering table 2,4

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,12 +1,51 @@
+class Expression {
+  // static create(first: number,second: number){
+  //   return {firstMultiplicator: first, secondMultiplicator: second }
+  // }
+
+  constructor(public firstMultiplicator: number, public secondMultiplicator: number) { }
+}
+
 function isIntegerBetween1and10(input: number): boolean {
   return (Number.isInteger(input) && input <= 10 && input >= 1) ? true : false
 }
 
-export class MultiplicationTable {
-  public render(start: number, end: number): string{
-    if(start > end) return ''
-    if(!isIntegerBetween1and10(start) || !isIntegerBetween1and10(end)) return ''
+function getCombination(start: number, end: number): Expression[] {
+  let i: number, j: number
+  const combination: Expression[] = []
+  for (i = start; i <= end; i++) {
+    for (j = start; j <= i; j++) {
+      const expression = new Expression(j, i)
+      combination.push(expression)
+    }
+  }
+  return combination
+}
 
-    return '1*1=1'
+function renderMultiplicationExpression(firstMultiplicator: number, secondMultiplicator: number, start: number, end: number) {
+  const header: string = firstMultiplicator===start ? '' : ' '
+  const tail: string = secondMultiplicator===firstMultiplicator ? '\n' : ' '
+  return `${header}${firstMultiplicator}*${secondMultiplicator}=${firstMultiplicator * secondMultiplicator}${tail}`
+}
+
+function renderCombinationExpression(start: number, end: number): string[] {
+  const combination = getCombination(start, end)
+  const combinationExpression = combination.map(expression => renderMultiplicationExpression(expression.firstMultiplicator, expression.secondMultiplicator, start, end))
+  return combinationExpression
+}
+
+function renderMultiplicationTable(combinationExpression: string[]): string {
+  return combinationExpression.join('').slice(0, -1)
+}
+
+export class MultiplicationTable {
+  public render(start: number, end: number): string {
+    if (start > end) return ''
+    if (!isIntegerBetween1and10(start) || !isIntegerBetween1and10(end)) return ''
+
+    const combinationExpression = renderCombinationExpression(start, end)
+    const multiplicationTable = renderMultiplicationTable(combinationExpression)
+
+    return multiplicationTable
   }
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -1,4 +1,4 @@
-import {MultiplicationTable} from '../src/MultiplicationTable'
+import { MultiplicationTable } from '../src/MultiplicationTable'
 
 describe('Multiplication Table', () => {
   it('should render multiplication table of (1, 1)', () => {
@@ -14,7 +14,7 @@ describe('Multiplication Table', () => {
     expect(rendered).toBe('1*1=1')
   })
 
-  it('should return empty string if start is larger than end', ()=> {
+  it('should return empty string if start is larger than end', () => {
     //given
     const table = new MultiplicationTable()
     const start = 2
@@ -27,7 +27,7 @@ describe('Multiplication Table', () => {
     expect(rendered).toBe('')
   })
 
-  it('should return empty string if start is not in range [1,10]', ()=> {
+  it('should return empty string if start is not in range [1,10]', () => {
     //given
     const table = new MultiplicationTable()
     const start = 0
@@ -40,7 +40,7 @@ describe('Multiplication Table', () => {
     expect(rendered).toBe('')
   })
 
-  it('should return empty string if end is not in range [1,10]', ()=> {
+  it('should return empty string if end is not in range [1,10]', () => {
     //given
     const table = new MultiplicationTable()
     const start = 1
@@ -53,7 +53,7 @@ describe('Multiplication Table', () => {
     expect(rendered).toBe('')
   })
 
-  it('should return empty string if start or end is not integer', ()=> {
+  it('should return empty string if start or end is not integer', () => {
     //given
     const table = new MultiplicationTable()
     const start = 1.1
@@ -64,5 +64,18 @@ describe('Multiplication Table', () => {
 
     //then
     expect(rendered).toBe('')
+  })
+
+  it('should return correct multiplication table for 2, 4', () => {
+    //given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 4
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('2*2=4\n2*3=6  3*3=9\n2*4=8  3*4=12  4*4=16')
   })
 })
