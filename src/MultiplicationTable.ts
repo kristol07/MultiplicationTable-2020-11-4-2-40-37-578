7e07bd7663b54e4e10c6656955605415da168ecc
Lin LIU <lliu41@slb.com>
2020-11-17 19:11:30 +0800

    Feat: Add validation for rule - input must be interger in [1,10]

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,6 +1,12 @@
+function isIntegerBetween1and10(input: number): boolean {
+  return (Number.isInteger(input) && input <= 10 && input >= 1) ? true : false
+}
+
 export class MultiplicationTable {
   public render(start: number, end: number): string{
     if(start > end) return ''
+    if(!isIntegerBetween1and10(start) || !isIntegerBetween1and10(end)) return ''
+
     return '1*1=1'
   }
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -26,4 +26,43 @@ describe('Multiplication Table', () => {
     //then
     expect(rendered).toBe('')
   })
+
+  it('should return empty string if start is not in range [1,10]', ()=> {
+    //given
+    const table = new MultiplicationTable()
+    const start = 0
+    const end = 1
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
+  })
+
+  it('should return empty string if end is not in range [1,10]', ()=> {
+    //given
+    const table = new MultiplicationTable()
+    const start = 1
+    const end = 11
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
+  })
+
+  it('should return empty string if start or end is not integer', ()=> {
+    //given
+    const table = new MultiplicationTable()
+    const start = 1.1
+    const end = 2
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
+  })
 })
