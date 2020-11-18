24851a4951f1d1c5118d052a7bdf0f56b1856544
Lin LIU <lliu41@slb.com>
2020-11-18 10:47:25 +0800

    Refactor: adjust for each equation before join

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,61 +1,55 @@
 class Expression {
-  // static create(first: number,second: number){
-  //   return {firstMultiplicator: first, secondMultiplicator: second }
-  // }
+  constructor(public first: number, public second: number) { }
+}
 
-  constructor(public firstMultiplicator: number, public secondMultiplicator: number) { }
+function generateExpressionbyString(expression: string): Expression {
+  const former = expression.split('=')[0]
+  const first = Number(former.split('*')[0])
+  const second = Number(former.split('*')[1])
+  return new Expression(first, second)
 }
 
 function isIntegerBetween1and10(input: number): boolean {
   return (Number.isInteger(input) && input <= 10 && input >= 1) ? true : false
 }
 
-function getCombination(start: number, end: number): Expression[] {
+function getCombination(start: number, end: number): string[] {
   let i: number, j: number
-  const combination: Expression[] = []
+  const combination: string[] = []
   for (i = start; i <= end; i++) {
     for (j = start; j <= i; j++) {
-      const expression = new Expression(j, i)
+      const expression = `${j}*${i}=${j * i}`
       combination.push(expression)
     }
   }
   return combination
 }
 
-function renderMultiplicationExpression(firstMultiplicator: number, secondMultiplicator: number, start: number) {
-  const header: string = firstMultiplicator === start ? '' : ' '
-  const tail: string = secondMultiplicator === firstMultiplicator ? '\n' : ' '
-  return `${header}${firstMultiplicator}*${secondMultiplicator}=${firstMultiplicator * secondMultiplicator}${tail}`
+function getIndex(first: number, second: number, start: number) {
+  return first - start + (1 + second - start) * (second - start) / 2
 }
 
-function renderCombinationExpression(start: number, end: number): string[] {
+
+function renderMultiplicationExpression(expressions: string[], start: number, end: number): string[] {
+  return expressions.map(expression => {
+    const exp = generateExpressionbyString(expression)
+    const first = exp.first
+    const second = exp.second
+
+    const targetIndex = getIndex(first, end, start)
+    const targetLength = expressions[targetIndex].length
+    const adjust = ' '.repeat(targetLength - expression.length)
+
+    const header: string = first === start ? '' : ' '
+    const tail: string = second === first ? '\n' : ' ' + adjust
+
+    return `${header}${first}*${second}=${first * second}${tail}`
+  })
+}
+
+function renderMultiplicationTable(start: number, end: number): string {
   const combination = getCombination(start, end)
-  const combinationExpression = combination.map(
-    expression => renderMultiplicationExpression(expression.firstMultiplicator, expression.secondMultiplicator, start))
-  return combinationExpression
-}
-
-function renderMultiplicationTable(combinationExpression: string[]): string {
-  return combinationExpression.join('').slice(0, -1)
-}
-
-function adjustTable(table: string): string {
-  const lines: string[] = table.split('\n')
-  const lastLine = lines[lines.length - 1]
-  const standardLength = lastLine?.split('  ').map(expression => expression.length)
-
-  return lines.map(line => {
-    const expressions = line.split('  ')
-    const expressionsLength = line.split('  ').map(expression => expression.length)
-    const diff: number[] = []
-    for (let i = 0; i < expressionsLength.length; i++) {
-      diff.push(standardLength[i] - expressionsLength[i])
-    }
-    for (let i = 0; i < expressions.length; i++) {
-      expressions[i] = expressions[i] + '  ' + ' '.repeat(diff[i])
-    }
-    return expressions.join('').trimEnd()
-  }).join('\n')
+  return renderMultiplicationExpression(combination, start, end).join('').slice(0, -1)
 }
 
 export class MultiplicationTable {
@@ -63,9 +57,8 @@ export class MultiplicationTable {
     if (start > end) return ''
     if (!isIntegerBetween1and10(start) || !isIntegerBetween1and10(end)) return ''
 
-    const combinationExpression = renderCombinationExpression(start, end)
-    const multiplicationTable = renderMultiplicationTable(combinationExpression)
+    const multiplicationTable = renderMultiplicationTable(start, end)
 
-    return adjustTable(multiplicationTable)
+    return multiplicationTable
   }
 }
